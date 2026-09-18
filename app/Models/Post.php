d89c071afcde3ae7aa4de3e18f45df16f48498d2
Tatjana Zaharova <zaharovatatjana83@gmail.com>
2026-09-18 15:38:24 +0200

    add factory for posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -27,6 +28,7 @@ use App\Models\Like;
     ])]
 class Post extends Model
 {
+    use HasFactory;
     /**
      * Get the user that owns the post.
      */
