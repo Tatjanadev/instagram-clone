7eeb39e687a6e27a35c4c9ff2d7e35b411d779a2
Tatjana Zaharova <zaharovatatjana83@gmail.com>
2026-09-18 15:12:27 +0200

    Update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'profile_photo_path' => fake()->optional(0.7)->randomElement([
+                'profile-images/user-1.jpg',
+                'profile-images/user-2.jpg',
+                'profile-images/user-3.jpg',
+            ]),
+            'bio' => fake()->optional(0.7)->sentence(),
+            'gender' => fake()->randomElement(['male', 'female']),
+            'date_of_birth' => fake()->optional(0.7)->dateTimeBetween('-50 years', '-18 years'),
             'remember_token' => Str::random(10),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
