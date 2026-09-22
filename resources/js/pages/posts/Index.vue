5f3ce024c8543d32931496e7847ae73dd152cbf3
Tatjana Zaharova <zaharovatatjana83@gmail.com>
2026-09-22 13:38:01 +0200

    add posts index page

---
+++ resources/js/pages/posts/Index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    posts: Array<{
+        id: number;
+        title: string;
+        description: string | null;
+    }>;
+}>();
+</script>
+
+<template>
+    <div>
+        <h1>Posts</h1>
+
+        <div v-if="posts.length === 0">
+            No posts yet.
+        </div>
+
+        <div v-else>
+            <div v-for="post in posts" :key="post.id">
+                <h2>{{ post.title }}</h2>
+                <p>{{ post.description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
