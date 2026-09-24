019f29f9ca9990617aa7a9b1dbcb4af9569bc44b
Tatjana Zaharova <zaharovatatjana83@gmail.com>
2026-09-24 11:32:47 +0200

    use demo images in post factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Post;
 use App\Models\Comment;
-use App\Models\Like;
-use App\Models\PostImage;
 use App\Models\Follow;
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\PostImage;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,30 +21,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $users =User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-       $posts = Post::factory(30)
-    ->state(function () use ($users) {
-        return [
-            'user_id' => $users->random()->id,
-        ];
-    })
-    ->create();
+        $posts = Post::factory(30)
+            ->state(function () use ($users) {
+                return [
+                    'user_id' => $users->random()->id,
+                ];
+            })
+            ->create();
 
-        $posts->each(function (Post $post) {
-            PostImage::factory(rand(1, 5))->create([
+        $demoImages = collect(Storage::disk('public')->files('demo/posts'))
+            ->filter(fn ($path) => in_array(
+                strtolower(pathinfo($path, PATHINFO_EXTENSION)),
+                ['jpg', 'jpeg', 'png', 'webp']
+            ))
+            ->values();
+
+        $posts->each(function (Post $post, int $index) use ($demoImages) {
+            PostImage::factory()->create([
                 'post_id' => $post->id,
+                'image_path' => $demoImages[$index],
             ]);
         });
 
         Comment::factory(60)
-        ->state(function () use ($users, $posts) {
-            return [
-                'user_id' => $users->random()->id,
-                'post_id' => $posts->random()->id,
-            ];
-        })
-        ->create();
+            ->state(function () use ($users, $posts) {
+                return [
+                    'user_id' => $users->random()->id,
+                    'post_id' => $posts->random()->id,
+                ];
+            })
+            ->create();
 
         $users->each(function (User $user) use ($posts) {
             $postsToLike = $posts->random(fake()->numberBetween(3, 8));
@@ -58,8 +67,8 @@ class DatabaseSeeder extends Seeder
 
         $users->each(function (User $user) use ($users) {
             $usersToFollow = $users
-            ->where('id', '!=', $user->id)
-            ->random(fake()->numberBetween(2, 5));
+                ->where('id', '!=', $user->id)
+                ->random(fake()->numberBetween(2, 5));
 
             $usersToFollow->each(function (User $userToFollow) use ($user) {
                 Follow::factory()->create([
@@ -68,7 +77,6 @@ class DatabaseSeeder extends Seeder
                 ]);
             });
         });
-
 
     }
 }
