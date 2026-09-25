4ed1b60eb9a4dd2508734499500ae2fbdca7e22e
Tatjana Zaharova <zaharovatatjana83@gmail.com>
2026-09-25 15:47:46 +0200

    Apply Tailwind class suggestions

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -39,7 +39,7 @@ defineOptions({
             </div>
         </div>
         <div
-            class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min"
+            class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-screen flex-1 rounded-xl border md:min-h-min"
         >
             <PlaceholderPattern />
         </div>
